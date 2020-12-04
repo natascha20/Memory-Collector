8f1627094cd29622cabefd12b992881c2f2a3fe3
Natascha Müller <natascha@Nataschas-MacBook-Pro.local>
2020-12-04 15:13:55 +0100

    Code & updated Plannung

---
+++ memory_collector/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+     appBar: AppBar(
+       backgroundColor: Colors.blue[100],
+       title: Text('Memory-Collector'),
+     ),
+    ),
+  ));
+}
