b69ff0ecf436c783d10f465b386b86d7b3db0d5c
Natascha Müller <natascha@nataschas-mbp.home>
2020-12-16 15:38:10 +0100

    Image Libary, button to add images, calender

--- memorycollector/lib/main.dart
+++ memorycollector/lib/main.dart
@@ -2,12 +2,15 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 String title = "";
 String summary = "";
 DateTime date;
 String location = "";
-List post;
+
+var post = new List(3);
+
 void setState(Null Function() param0) {}
 
 void main() {
@@ -34,83 +37,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class New extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).accentColor,
-      appBar: AppBar(
-        title: Text('New'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).accentColor,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      MyDatePicker(),
-                      MyCustomForm(),
-                      ElevatedButton(
-                        child: Text('Home'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      ElevatedButton(
-                        child: Text('Location'),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Location()),
-                          );
-                        },
-                      )
-                    ],
-                  ),
-                )),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Location extends StatelessWidget {
-  int _selectedIndex = 2;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).accentColor,
-      appBar: AppBar(
-        title: Text('Locations'),
-      ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            child: Text('Home'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          ElevatedButton(
-            child: Text('New'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          )
-        ],
-      ),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
@@ -120,45 +46,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
+  Future<File> imageFile;
+
+  pickImageFromGallery(ImageSource source){
+    setState((){
+      // ignore: deprecated_member_use
+      imageFile = ImagePicker.pickImage(source: source);
+    });
+  }
+
+  Widget showImage(){
+    return FutureBuilder<File>(
+      future: imageFile,
+      builder: (BuildContext context, AsyncSnapshot<File> snapshot){
+        if(snapshot.connectionState == ConnectionState.done && snapshot.data != null){
+          return Image.file(
+            snapshot.data,
+            width: 300,
+            height: 300,
+          );
+        } else if (snapshot.error != null){
+          return const Text('Error picking image');
+        } else{
+          return const Text('No image selected');
+        }
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).accentColor,
-        appBar: AppBar(
-          title: Text(
-            widget.title,
-            style: TextStyle(
-              color: Theme.of(context).accentColor,
-              fontWeight: FontWeight.bold,
-            ),
+      backgroundColor: Theme.of(context).accentColor,
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            color: Theme.of(context).accentColor,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        body: Column(
-          children: [
-            ElevatedButton(
-              child: Text('New'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => New()),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: Text('Location'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Location()),
-                );
-              },
-            ),
-          ],
-        ),
+      ),
+      body: Column(
+        children: [
+          //   ElevatedButton(
+          //     child: Text('New'),
+          //     onPressed: () {
+          //       Navigator.push(
+          //         context,
+          //         MaterialPageRoute(builder: (context) => New()),
+          //       );
+          //     },
+          //   ),
+          //   ElevatedButton(
+          //     child: Text('Location'),
+          //     onPressed: () {
+          //       Navigator.push(
+          //         context,
+          //         MaterialPageRoute(builder: (context) => Location()),
+          //       );
+          //     },
+          //   ),
+          showImage(),
+          RaisedButton(
+            child: Text('select from gallery'),
+            onPressed: (){
+              pickImageFromGallery(ImageSource.gallery);
+            },
+          ),
+          MyDatePicker(),
+          MyCustomForm(),
+          Text('$post'),
+        ],
+      ),
     );
-
   }
 }
 
@@ -204,15 +165,14 @@ class MyCustomFormState extends State<MyCustomForm> {
           ),
           ElevatedButton(
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).primaryColor),
             ),
             child: Text('save'),
             onPressed: () {
-              if (_formKey.currentState.validate()) {
-                Scaffold.of(context);
-              }
-              Navigator.pop(context);
-              List post = [title, summary, location];
+              post = [title, summary, location];
+              Scaffold.of(context);
+              print(post);
             },
           ),
         ],
@@ -221,36 +181,36 @@ class MyCustomFormState extends State<MyCustomForm> {
   }
 }
 
-class MyDatePicker extends StatefulWidget{
+class MyDatePicker extends StatefulWidget {
   @override
   MyDatePickerState createState() {
     return MyDatePickerState();
   }
 }
 
-class MyDatePickerState extends State<MyDatePicker>{
+class MyDatePickerState extends State<MyDatePicker> {
   @override
   Widget build(BuildContext context) {
     DateTime _dateTime;
     return Column(
-            children: <Widget>[
-              RaisedButton(
-                child: Text('Pick a date'),
-                onPressed: (){
-                  showDatePicker(
-                    context: context,
-                    initialDate: _dateTime == null ? DateTime.now() : _dateTime,
-                    firstDate: DateTime(2010),
-                    lastDate: DateTime(2021),
-                  ).then((date) {
-                    setState((){
-                      _dateTime = date;
-                    });
-                  });
-                },
-              ),
-              Text('$_dateTime'),
-            ],
+      children: <Widget>[
+        RaisedButton(
+          child: Text('Pick a date'),
+          onPressed: () {
+            showDatePicker(
+              context: context,
+              initialDate: _dateTime == null ? DateTime.now() : _dateTime,
+              firstDate: DateTime(2010),
+              lastDate: DateTime(2021),
+            ).then((date) {
+              setState(() {
+                _dateTime = date;
+              });
+            });
+          },
+        ),
+        Text('$_dateTime'),
+      ],
     );
   }
 }
